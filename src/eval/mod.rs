f2c5b3a901bfe1f0effda7155d5b213c7ba36669
jangjunha <jangjunha113@gmail.com>
2021-05-28 10:43:06 +0900

    Add empty eval module

---
+++ src/eval/mod.rs
@@ -0,0 +1,17 @@
+use crate::parse::exp;
+use crate::parse::expression::Exp;
+
+
+// fn eval(e: Exp) {
+//     match e {
+//         Exp::Atom() => {},
+//         Exp::Cond => {},
+//         Exp::Iff() => {},
+//         Exp::And() => {},
+//         Exp::Or() => {},
+//         Exp::Neg() => {},
+//         Exp::UnivGenr() => {},
+//         Exp::ExistGenr() => {},
+//         Exp::Falsum() => {},
+//     }
+// }
