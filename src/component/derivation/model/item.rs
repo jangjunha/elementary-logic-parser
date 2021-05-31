cfb878a500775f2172533f5482f60a2236dde893
jangjunha <jangjunha113@gmail.com>
2021-05-31 18:07:16 +0900

    Serialize item sentence as regularized form

--- src/component/derivation/model/item.rs
+++ src/component/derivation/model/item.rs
@@ -1,14 +1,32 @@
-use serde::{Deserialize, Serialize};
+use serde::ser::{Serialize, SerializeStruct, Serializer};
+use serde::Deserialize;
 
 use super::rule::DerivationRule;
+use crate::parse::model::to_string as exp_to_string;
 use crate::parse::{exp as parse_exp, Exp};
 
-// TODO: Serialize 시 sentence_text 그대로 넣지 말고 ``create::parse::model::to_string(sentence())`` 쓰기
-#[derive(Debug, Clone, Serialize, Deserialize, Hash, Eq, PartialEq)]
+#[derive(Debug, Clone, Deserialize, Hash, Eq, PartialEq)]
 pub struct DerivationItem {
     pub id: i32,
     pub sentence_text: String,
     pub rule: Option<DerivationRule>,
+}
+
+impl Serialize for DerivationItem {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("DerivationItem", 3)?;
+        state.serialize_field("id", &self.id)?;
+        if let Ok(exp) = self.sentence() {
+            state.serialize_field("sentence_text", &exp_to_string(&exp))?;
+        } else {
+            state.skip_field("sentence_text")?;
+        }
+        state.serialize_field("rule", &self.rule)?;
+        state.end()
+    }
 }
 
 impl DerivationItem {
