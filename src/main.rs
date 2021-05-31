94699ecde6667680eac5b917011e6f9b5b50688d
jangjunha <jangjunha113@gmail.com>
2021-05-31 21:35:19 +0900

    Show panics to browser console

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ mod eval;
 mod parse;
 mod service;
 
+extern crate console_error_panic_hook;
+
+use std::panic;
 use yew;
 
 use component::app::App;
 
 fn main() {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
     yew::start_app::<App>();
 }
